8be170d2850f4e6644410d867962360319a8362d
varunchitturi <varunchitturi@icloud.com>
2020-08-02 22:21:15 -0700

    Add Home link

---
+++ Index/script.js
@@ -0,0 +1,5 @@
+function goToIndex() {
+    window.location.href = 'index.html';
+}
+document.querySelector("#site-name").addEventListener("click", goToIndex);
+document.querySelector("#site-icon").addEventListener("click", goToIndex);
