3bf3655257a8a0dcca95e45fe9873dc5995ab5ce
varunchitturi <varunchitturi@icloud.com>
2020-08-04 06:45:34 -0500

    more sql changes

--- SignUp/script.js
+++ SignUp/script.js
@@ -1,6 +1,9 @@
-
+const mysql = require('mysql');
 function login(){
     window.location.href = "../Account/account.html";
 }
 document.getElementById("log-in-button").addEventListener("click",login);
+
+
+
 
