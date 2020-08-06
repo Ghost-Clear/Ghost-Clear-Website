dd04a83e544581abccb29eee7b404e00e1dcc2d8
varunchitturi <varunchitturi@icloud.com>
2020-08-06 06:04:54 -0500

    Add main title and animations

--- Shop/script.js
+++ Shop/script.js
@@ -1,8 +1,9 @@
 function goToIndex() {
-    window.location.href = 'index.html';
+    window.location.href = '../index.html';
 }
+document.querySelector("#site-name").addEventListener("click", goToIndex);
 document.querySelector("#site-icon").addEventListener("click", goToIndex);
 function goToShop(){
-    window.location.href = 'Shop/shop.html';
+    window.location.href = 'shop.html';
 }
 document.querySelector("#shop").addEventListener("click", goToShop);
