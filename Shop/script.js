4d7e53c57150214a9c164efaa8868b05d5a85297
varunchitturi <varunchitturi@icloud.com>
2020-08-07 07:47:33 -0500

    Minor UI adjustments

--- Shop/script.js
+++ Shop/script.js
@@ -1,7 +1,7 @@
 function goToIndex() {
     window.location.href = '../index.html';
 }
-document.querySelector("#site-name").addEventListener("click", goToIndex);
+
 document.querySelector("#site-icon").addEventListener("click", goToIndex);
 function goToShop(){
     window.location.href = 'shop.html';
