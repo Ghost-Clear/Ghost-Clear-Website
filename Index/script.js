d0b8197b8f65b707502081b2e3854b7d416f2d4d
varunchitturi <varunchitturi@icloud.com>
2020-08-08 03:46:18 -0500

    Add text animation.

--- Index/script.js
+++ Index/script.js
@@ -8,5 +8,31 @@ function goToShop(){
     window.location.href = 'Shop/shop.html';
 }
 document.querySelector("#shop").addEventListener("click", goToShop);
+document.querySelector('#text-one').classList.add('hidden');
+(function() {
+    var elements;
+    var windowHeight;
 
+    function init() {
+        elements = document.querySelectorAll('#text-one');
+        windowHeight = window.innerHeight;
+    }
 
+    function checkPosition() {
+        for (let i = 0; i < elements.length; i++) {
+            let element = elements[i];
+            let positionFromTop = elements[i].getBoundingClientRect().top;
+
+            if (positionFromTop - windowHeight <= -100) {
+                element.classList.add('text-animateLR');
+                element.classList.remove('hidden');
+            }
+        }
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+
+    init();
+    checkPosition();
+})();
